4374ba6192b960cd5e02ea5d9dff84160a37a641
Philip Nichols <philipdnichols@gmail.com>
2013-08-02 19:03:59 -0400

    Check in for Quill 18's first tutorial

---
+++ Quill18 Creates/Unity Tutorial Project/Assets/Scripts/BallScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		rigidbody.AddForce(100f, 300f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	
+	}
+}
---
+++ Quill18 Creates/Unity Tutorial Project/Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetAxis("Horizontal") < 0) {
+			Debug.Log("Left");
+			
+			transform.Translate(-10.0f * Time.deltaTime, 0, 0);
+		} else if (Input.GetAxis("Horizontal") > 0) {
+			Debug.Log("Right");
+			
+			transform.Translate(10.0f * Time.deltaTime, 0, 0);
+		}
+		
+//		if (Input.GetButton("Jump")) {
+//			Debug.Log("Jump");
+//		}
+	}
+}
