c51abfe9ab547e29a428ec019e08c2dc05adff2d
Philip Nichols <philipdnichols@gmail.com>
2013-08-04 13:45:43 -0400

    Quill 18 Creates End of Procedural Generation Tutorial Series

--- Quill18 Creates/Procedural Mesh Generation - Sim City Roads/Assets/Scripts/GroundClick.cs
+++ Quill18 Creates/Procedural Mesh Generation - Sim City Roads/Assets/Scripts/GroundClick.cs
@@ -4,29 +4,68 @@ using System.Collections;
 public class GroundClick : MonoBehaviour {
 	
 	public GameObject roadPrefab;
+	public GameObject nodePrefab;
 	
-	Vector3 roadStart;
+	GameObject nodeStart;
 	
 	void Update() {
 		if (Input.GetMouseButtonDown(0)) {
-			ClickLocation(out roadStart);
+			Vector3 roadStart;
+			if (ClickLocation(out roadStart)) {
+				nodeStart = (GameObject) Instantiate(nodePrefab, roadStart, Quaternion.identity);
+				nodeStart.GetComponent<NodeClick>().ground = this;
+			}
 		}
 		
 		if (Input.GetMouseButtonUp(0)) {
 			Vector3 roadEnd;
-			if (ClickLocation(out roadEnd)) {
-				CreateRoad(roadStart, roadEnd);
+			if (nodeStart != null && ClickLocation(out roadEnd)) {
+				GameObject nodeEnd = (GameObject) Instantiate(nodePrefab, roadEnd, Quaternion.identity);
+				nodeEnd.GetComponent<NodeClick>().ground = this;
+				
+				CreateRoad(nodeStart.transform.position, nodeEnd.transform.position);
+			} else if (nodeStart != null && ClickLocationNode(out roadEnd)) {
+				CreateRoad(nodeStart.transform.position, roadEnd);
 			}
+			
+			nodeStart = null;
 		}
+	}
+	
+	public void SetNodeStart(GameObject n) {
+		nodeStart = n;
+	}
+	
+	public void SetNodeEnd(GameObject n) {
+		CreateRoad(nodeStart.transform.position, n.transform.position);
+		nodeStart = null;
 	}
 	
 	bool ClickLocation(out Vector3 point) {
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		
 		RaycastHit hitInfo;
-		if (collider.Raycast(ray, out hitInfo, Mathf.Infinity)) {
-			point = hitInfo.point;
-			return true;
+		if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity)) {
+			if (hitInfo.collider == collider) {
+				point = hitInfo.point;
+				return true;
+			}
+		}
+		
+		// TODO this is hacky
+		point = Vector3.zero;
+		return false;
+	}
+	
+	bool ClickLocationNode(out Vector3 point) {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+		RaycastHit hitInfo;
+		if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity)) {
+			if (hitInfo.collider.transform.parent != null && hitInfo.collider.transform.parent.tag == "Node") {
+				point = hitInfo.collider.transform.position;
+				return true;
+			}
 		}
 		
 		// TODO this is hacky
@@ -35,58 +74,60 @@ public class GroundClick : MonoBehaviour {
 	}
 	
 	void CreateRoad(Vector3 roadStart, Vector3 roadEnd) {
-		GameObject road = (GameObject) Instantiate(roadPrefab);
-		road.transform.position = roadStart + Vector3.up * 0.01f;
-		
-		// Doesn't work
-//		float angle = Vector3.Angle(Vector3.right, roadEnd - roadStart);
-//		Debug.Log(angle);
-//		road.transform.rotation = Quaternion.Euler(0, angle, 0);
-		
-		road.transform.rotation = Quaternion.FromToRotation(Vector3.right, roadEnd - roadStart);
-		Debug.Log(road.transform.rotation.eulerAngles);
-		
 		float width = 1.0f;
 		float length = Vector3.Distance(roadStart, roadEnd);
 		
-		Vector3[] vertices = {
-			new Vector3(0, 		0, -width / 2.0f),
-			new Vector3(length, 0, -width / 2.0f),
-			new Vector3(length, 0,  width / 2.0f),
-			new Vector3(0, 		0,  width / 2.0f)
-		};
+		if (length >= 1.0f) {
+			GameObject road = (GameObject) Instantiate(roadPrefab);
+			road.transform.position = roadStart + Vector3.up * 0.01f;
 		
-		int[] triangles = {
-			1, 0, 2,	// triangle 1
-			2, 0, 3		// triangle 2
-		};
-		
-		Vector2[] uv = {
-			new Vector2(0, 0),
-			new Vector2(length, 0),
-			new Vector2(length, 1),
-			new Vector2(0, 1)
-		};
-		
-		Vector3[] normals = {
-			Vector3.up,
-			Vector3.up,
-			Vector3.up,
-			Vector3.up
-		};
-		
-		Mesh mesh = new Mesh();
-		mesh.vertices = vertices;
-		mesh.triangles = triangles;
-		mesh.uv = uv;
-		mesh.normals = normals;
-		mesh.Optimize();
-		
-		MeshFilter meshFilter = road.GetComponent<MeshFilter>();
-		meshFilter.mesh = mesh;
-		
-		// BAD
-//		Vector2 texScale = new Vector2(length, 1);
-//		road.GetComponent<MeshRenderer>().material.mainTextureScale = texScale;
+			// Doesn't work
+	//		float angle = Vector3.Angle(Vector3.right, roadEnd - roadStart);
+	//		Debug.Log(angle);
+	//		road.transform.rotation = Quaternion.Euler(0, angle, 0);
+			
+			road.transform.rotation = Quaternion.FromToRotation(Vector3.right, roadEnd - roadStart);
+			Debug.Log(road.transform.rotation.eulerAngles);
+			
+			Vector3[] vertices = {
+				new Vector3(0, 		0, -width / 2.0f),
+				new Vector3(length, 0, -width / 2.0f),
+				new Vector3(length, 0,  width / 2.0f),
+				new Vector3(0, 		0,  width / 2.0f)
+			};
+			
+			int[] triangles = {
+				1, 0, 2,	// triangle 1
+				2, 0, 3		// triangle 2
+			};
+			
+			Vector2[] uv = {
+				new Vector2(0, 0),
+				new Vector2(length, 0),
+				new Vector2(length, 1),
+				new Vector2(0, 1)
+			};
+			
+			Vector3[] normals = {
+				Vector3.up,
+				Vector3.up,
+				Vector3.up,
+				Vector3.up
+			};
+			
+			Mesh mesh = new Mesh();
+			mesh.vertices = vertices;
+			mesh.triangles = triangles;
+			mesh.uv = uv;
+			mesh.normals = normals;
+			mesh.Optimize();
+			
+			MeshFilter meshFilter = road.GetComponent<MeshFilter>();
+			meshFilter.mesh = mesh;
+			
+			// BAD
+	//		Vector2 texScale = new Vector2(length, 1);
+	//		road.GetComponent<MeshRenderer>().material.mainTextureScale = texScale;
+		}
 	}
 }
---
+++ Quill18 Creates/Procedural Mesh Generation - Sim City Roads/Assets/Scripts/NodeClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeClick : MonoBehaviour {
+	
+	public GroundClick ground;
+	
+	void OnMouseDown() {
+		ground.SetNodeStart(gameObject);
+	}
+}
